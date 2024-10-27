2fc6040d555e792ef40cc3f35ea0c50d1cd0746f
Anto462 <94753796+Anto462@users.noreply.github.com>
2024-10-26 20:20:50 -0600

    Se añaden recursos y carpeta principal
    Se añade la carpeta de recursos y la base de los mapas que tendrá el juego

---
+++ LaHistoriaDeLaProgra/Script/constants/constants-stringtable.js
@@ -0,0 +1,335 @@
+
+var StringTable = {
+	LoadSave_SaveQuestion: "¿Guardar en este archivo?",
+	LoadSave_NoLoad: "No hay archivo para cargar.",
+	LoadSave_NoData: "sin dato",
+	LoadSave_Chapter: "Capítulo",
+	LoadSave_SaveFileMark: "Archivo",
+	LoadSave_Rest: "Base",
+	
+	Communication_NoData: "No hay dato disponible.",
+	
+	GameStart_DifficultySelect: "Elija el nivel de dificultad del juego.",
+	GameStart_ClearPointDescription: "Puede continuar los artículos usando los puntos de juego completo.\nEs recomendable hacerlo si quieres jugar en el mejor nivel.\nSus puntos de juego completo actuales son:",
+	GameStart_ClearPointQuestion: "¿Desea continuar los artículos?",
+	
+	UnitSortie_Start: "Press Space",
+	UnitSortie_Max: "Max. número de unidades permitidas para salir",
+	UnitSortie_Question: "¿Comenzar el combate?",
+	
+	Stock_Extract: "Saca artículos",
+	Stock_Store: "Almacena artículos",
+	Stock_AllStore: "Almacena todos los artículos",
+	
+	Marshal_StockOperation: "Cambia la unidad con la tecla de arriba/bajo",
+	Marshal_StockTrade: "Saca artículos obtenidos",
+	Marshal_ItemTrade: "Intercambia artículos entre unidades",
+	Marshal_UnitSort: "Coloca de nuevo las unidades",
+	Marshal_UnitStatus: "Comprueba estado de unidad",
+	Marshal_Shop: "Compra artículos",
+	Marshal_Bonus: "Intercambia bonus con artículos",
+	Marshal_ItemUse: "Usa artículos",
+	Marshal_ClassChange: "Cambia la clase de unidad",
+	
+	ItemUse_Unit: "Unidad",
+	ItemUse_Stock: "Existencias",
+	ItemUse_Question: "¿Desea usar este artículo?",
+	
+	ExperienceDistribution_CannotLevelup: "No se puede subir de nivel más",
+	ExperienceDistribution_BonusShortage: "Falta de bonificación",
+	
+	Extra_Character: "Los personajes aparecidos en el juego están en la lista.",
+	Extra_Word: "Los términos técnicos del juego están en la lista.",
+	Extra_Gallery: "Las imágenes mostradas en el juego están en la lista.",
+	Extra_SoundRoom: "La música reproducida en el juego está en la lista.",
+	Extra_Recollection: "Escenas mostradas en el juego están en la lista.",
+	
+	Config_MusicPlay: "Volumen Música",
+	Config_MusicPlayDescription: "Fijar volumen música",
+	Config_SoundEffect: "Volumen Efectos",
+	Config_SoundEffectDescription: "Fijar volumen efecto",
+	Config_RealBattle: "Batalla Real",
+	Config_RealBattleDescription: "Elegir mostrar movimiento de batalla mientras luchas",
+	Config_AutoTurnEnd: "Fin de Turno  Auto",
+	Config_AutoTurnEndDescription: "Elegir finalizar turno automáticamente",
+	Config_AutoTurnSkip: "Saltar Turno Auto",
+	Config_AutoTurnSkipDescription: "Elegir saltar turno del enemigo",
+	Config_MapGrid: "Casilla de Mapa",
+	Config_MapGridDescription: "Elegir mostrar casillas del mapa",
+	Config_UnitSpeed: "Velocidad de Unidad",
+	Config_UnitSpeedDescription: "Elegir velocidad de desplazamiento de unidad",
+	Config_MessageSpeed: "Velocidad de Mensaje",
+	Config_MessageSpeedDescription: "Elegir velocidad del texto",
+	Config_MapUnitWindow: "Ventana de Mapa Unidad",
+	Config_MapUnitWindowDescription: "Elegir mostrar la ventana de unidad detallada",
+	Config_LoadCommand: "Comando Cargar",
+	Config_LoadCommandDescription: "Elegir mostrar el comando cargar",
+	Config_AutoCursor: "Cursor Auto",
+	Config_AutoCursorDescription: "Elegir si el cursor va con el personaje que lidera",
+	Config_MouseOperation: "Manejar con Ratón",
+	Config_MouseOperationDescription: "Elegir habilitar manejo con ratón",
+	Config_MouseCursorTracking: "Seguimiento de Cursor",
+	Config_MouseCursorTrackingDescription: "Elegir habilitar seguimiento de cursor auto",
+	Config_Voice: "Volumen de Voz",
+	Config_VoiceDescription: "Elegir volumen de voz",
+	Config_RealBattleScaling: "Zoom Batalla Real",
+	Config_RealBattleScalingDescription: "Elegir amplificar escena de batalla real",
+	Config_ScrollSpeed: "Velocidad de Scroll",
+	Config_ScrollSpeedDescription: "Elegir velocidad de scroll al operar unidad",
+	Config_EnemyMarking: "Marcado Enemigo",
+	Config_EnemyMarkingDescription: "Elegir visualizar gama de movimientos al cancelar en el mapa",
+	Config_MapUnitHpVisible: "PV de Unidad en Mapa ",
+	Config_MapUnitHpVisibleDescription: "Visualiza PV de unidad en el mapa",
+	Config_MapUnitSymbol: "Símbolo en Mapa",
+	Config_MapUnitSymbolDescription: "Muestra dónde se encuentra cada unidad en el mapa",
+	Config_DamagePopup: "Aviso de daño",
+	Config_DamagePopupDescription: "Muestra el daño realizado al enemigo en batalla",
+	Config_SkipControl: "Control de Salto",
+	Config_SkipControlDescription: "Elegir dónde anular la opción de salto",
+	
+	ItemChange_UnitItemFull: "La unidad tiene demasiados artículos.\nPulse OK o Cancelar.",
+	ItemChange_TradeTitle: "¿Desea intercambiarlo por este artículo?",
+	ItemChange_StockSendTitle: "¿Desea mandar el artículo obtenido a existencias ?",
+	ItemChange_StockThrowTitle: "¿Desea descartar el artículo obtenido?",
+	ItemChange_StockItemFull: "Las existencias están llenas.\nIntercambie o descarte artículo.",
+	ItemChange_TargetItem: "Objetivo Artículo",
+	
+	BattleSetup_NoPlayer: "No hay ningún jugador.\nEl juego está acabado.",
+	
+	BattleResult_GetGold: "Oro",
+	BattleResult_GetBonus: "Bonus",
+	BattleResult_GetItem: "Artículo",
+	
+	Rest_Next: "¿Desea terminar esta base?",
+	
+	Quest_Select: "¿Desea ir a este mapa?",
+	Quest_EnemyTotal: "Enemigos ",
+	Quest_AverageLevel: "Nivel Medio de Enemigos ",
+	Quest_GetGold: "Oro",
+	Quest_GetBonus: "Bonus",
+	Quest_GetItem: "Get!",
+	
+	ClassChange_UnitClassChange: "¿Desea cambiar a esta clase?",
+	ClassChange_UnableClassChange: "Esta unidad no puede cambiar la clase.",
+	ClassChange_UnableClassChangeMore: "Esta unidad no puede cambiar la clase más.",
+	ClassChange_UnableClassChangeItem: "Esta unidad no puede cambiar la clase \n con este artículo.",
+	
+	Metamorphoze_Change: "¿Desea metamorfosear esta unidad?",
+	
+	ShopLayout_SelectBuy: "Compra",
+	ShopLayout_SelectSell: " Vende",
+	ShopLayout_Unit: "Unidad",
+	ShopLayout_Stock: "Existencias",
+	
+	ResurrectionLayout_Empty: "No hay unidad para revivir",
+	ResurrectionLayout_Question: "¿Desea revivir esta unidad?",
+	
+	UnitSummary_Alive: "Vivo",
+	UnitSummary_Death: "Muerto",
+	UnitSummary_Injury: "Herido",
+	UnitSummary_Erase: "Borrado",
+	UnitSummary_Immortal: "Inmortal",
+	
+	SkillList_Owner: "Poseedor",
+	
+	EventTest_End: "El evento de Memorias se ha terminado.",
+	EventTest_Key: "El juego se cierra pulsando OK o Cancelar.",
+	
+	QuestionWindow_DefaultCase1: "Sí",
+	QuestionWindow_DefaultCase2: "No",
+	
+	InfoWindow_Information: "Aviso",
+	InfoWindow_Warning: "Advertencia",
+	
+	ReverseWeapon_Magic: "Arma de Magia",
+	ReverseWeapon_Physics: "Arma de Física",
+	
+	MapPosChoose_Unit: "¿Acepta esta unidad?",
+	MapPosChoose_Pos: "¿Acepta esta posición?",
+	
+	Discard_Warning: "No puede descartar este artículo.",
+	
+	StockItem_ItemDiscard: "¿Descarta este artículo?",
+	StockItem_Extract: "Coger",
+	StockItem_Store: "Guardar",
+	StockItem_AllStore: "Vaciar",
+	
+	ItemWork_Equipment: "Equip",
+	ItemWork_Use: "Uso",
+	ItemWork_Discard: "Descartar",
+	
+	ItemInfo_Recovery: "Recuperación",
+	ItemInfo_EntireRecovery: "Recuperación Total",
+	ItemInfo_Damage: "Daño",
+	ItemInfo_ClassChange: "Cambio de Clase",
+	ItemInfo_Doping: "Dopaje",
+	ItemInfo_Resurrection: "Resurrección",
+	ItemInfo_SkillChange: "Aprender Habilidad",
+	ItemInfo_Quick: "De nuevo",
+	ItemInfo_Teleportation: "Teletransporte",
+	ItemInfo_Rescue: "Rescate",
+	ItemInfo_Key: "Abrir Cerrojo",
+	ItemInfo_Durability: "Reparar",
+	ItemInfo_Steal: "Robar",
+	ItemInfo_State: "Cambio de Estado",
+	ItemInfo_StateRecovery: "Recuperación de Estado",
+	ItemInfo_Switch: "Especial",
+	ItemInfo_Fusion: "Fusión",
+	ItemInfo_Metamorphoze: "Metamorfosis",
+	ItemInfo_Unusable: "Inutilizable ",
+	ItemInfo_Custom: "Personalizado",
+	
+	ItemWord_SuffixItem: " (Artículo)",
+	ItemWord_SuffixWand: " (Bastón)",
+	ItemWord_MultiAttack: " Ataques",
+	
+	SkillWord_Less: "Menos de % ",
+	SkillWord_Invocation: "Porcentaje",
+	FusionWord_Success: "Tasa",
+	
+	UnitSentence_Support: "-Seguidor-",
+	
+	UnitType_Player: "JUGADOR",
+	UnitType_Enemy: "ENEMIGO",
+	UnitType_Ally: "ALIADO",
+	
+	WeaponOption_HpAbsorb: "Absorción PV",
+	WeaponOption_NoGuard: "Anular Def",
+	WeaponOption_HpMinimum: "Reducir PV a 1",
+	WeaponOption_HalveAttack: "Reducir Ataque",
+	WeaponOption_HalveAttackBreak: "Anular ataque reducido",
+	WeaponOption_SealAttack: "Bloquear ataque",
+	WeaponOption_SealAttackBreak: "Anular bloqueo de ataque",
+	
+	DamageType_Fixed: "Fijo",
+	DamageType_Physics: "Física",
+	DamageType_Magic: "Mágico",
+	DamageType_Name: "Atributo",
+	
+	DurabilityType_Max: "Total",
+	DurabilityType_Half: "Medio",
+	DurabilityType_Break: "Deshecho",
+	
+	Chapter_Header: "Cap",
+	Chapter_Footer: "",
+	Chapter_SideStory: "ex",
+	Chapter_First: "Prólogo",
+	Chapter_Last: "Final",
+	Chapter_Rest: "*",
+	Chapter_Quest: "MS",
+	
+	Spped_Direct: "Instante",
+	Speed_High: "Rápido",
+	Speed_Normal: "Normal",
+	Speed_Low: "Lento",
+	
+	AutoTurnSkip_Direct: "Instante",
+	AutoTurnSkip_Quick: "Rápido",
+	AutoTurnSkip_None: "Nada",
+	
+	SkipControl_AllInput: "Todo",
+	SkipControl_Mouse: "Ratón",
+	SkipControl_None: "Nada",
+	
+	MapUnitHp_Number: "Valor",
+	MapUnitHp_Gauge: "Barra",
+	MapUnitHp_None: "Nada",
+	
+	Objective_Victory: "Condiciones de victoria",
+	Objective_Defeat: "Condiciones de derrota",
+	
+	Select_On: "On",
+	Select_Off: "Off",
+	
+	Range_Self: "Solo",
+	Range_All: "Todo",
+	
+	Recovery_Value: "Punto",
+	Recovery_All: "Total",
+	
+	Aggregation_SuffixEquipment: " Equipamiento",
+	Aggregation_SuffixPossession: " Posesión",
+	Aggregation_SuffixAddition: " Añadido",
+	Aggregation_Match: "Aplicable",
+	Aggregation_Mismatch: "No Aplicable",
+	Aggregation_MatchAll: "Todo Aplicable",
+	Aggregation_MismatchAll: "Ninguno Aplicable",
+	
+	Key_Target: "Objetivos",
+	Key_Gate: "Puerta",
+	Key_Treasure: "Tesoro",
+	
+	ItemLost: " está roto",
+	ItemSteal: " ha sido robado",
+	
+	ClassChangeInfo_Target: "Objetivos",
+	QuickInfo_Surroundings: 'Unidades alrededor',
+	Teleportation_Range: "Dis",
+	
+	SkillCategory_BattleAttack: "Tipo Ofensivo Batalla",
+	SkillCategory_BattleDefence: "Tipo Defensivo Batalla",
+	SkillCategory_BattleAllowed: "Tipo Aumento Hab. Batalla",
+	SkillCategory_Allowed: "Tipo aumento Hab. General",
+	SkillCategory_Command: "Tipo Comando",
+	SkillCategory_Action: "Tipo Acción",
+	SkillCategory_Custom: "Personalizado",
+	
+	GetTitle_ItemChange: "Obtener!",
+	GetTitle_SkillChange: "Obtener!",
+	GetTitle_GoldChange: "Obtener!",
+	GetTitle_BonusChange: "Obtener!",
+	
+	LostTitle_ItemChange: "Pérdida...",
+	LostTitle_SkillChange: "Pérdida...",
+	LostTitle_GoldChange: "Pérdida...",
+	LostTitle_BonusChange: "Pérdida...",
+	
+	GetTitle_ExperiencePlus: "Exp.",
+	GetTitle_NewSkill: "Nueva Habilidad!",
+	GetTitle_PowerupSkill: "Subir la fuerza!",
+	
+	UnitSimple_Target: "Objetivo ...",
+	
+	State_Addition: "Añadido",
+	State_Recovery: "Curación",
+	State_Regist: "Resistencia",
+	State_AllBadState: "Todos los Mal Estado",
+	
+	HideData_Secret: "secreto",
+	HideData_Question: "???",
+	HideData_Unknown: "Desconocido",
+	
+	AttackMenu_AttackCount: "Ataques ",
+	
+	PlayerActive: "Jugadores",
+	
+	SkipProgress: "Saltarse ...",
+	
+	PlayTime: 'Play Time',
+	
+	Status_Level: "Niv",
+	Status_Experience: "Ex",
+	
+	Signal_Turn: "TURNO",
+	Signal_Gold: "ORO",
+	Signal_Bonus: "BONUS",
+	
+	CurrencySign_Gold: "O",
+	CurrencySign_Bonus: "B",
+	CurrencySign_Point: "P",
+	
+	SignWord_Multiple: "x",
+	SignWord_Percent: "%",
+	SignWord_WaveDash: "a",
+	SignWord_Limitless: "--"
+};
+
+var ContentLayout = {
+	ITEM_SPACE: 66,
+	KEYWORD_HEIGHT: 1
+};
+
+var HorizontalLayout = {
+	OBJECT_SPACE: 5,
+	OBJECT_WIDTH: 14
+};
